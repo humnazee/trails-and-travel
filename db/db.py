8fb169049fcbacb22e4951db71a7d7c117c5649c
humnazee <zeeshan.humna@hotmail.com>
2023-05-16 11:17:53 +0930

    trails-and-travel

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 import psycopg2
 import psycopg2.extras
-
-DB_URL = "dbname=travel_app"
+import os
+DB_URL = os.environ.get("DATABASE_URL", "dbname=travel_app")
 
 def sql(query, parameters=[]):
   connection = psycopg2.connect(DB_URL) # open connection
